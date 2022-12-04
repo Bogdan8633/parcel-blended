9cfa1be345a0dd73e5a612f87fa35568637d5e6c
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2022-12-04 20:39:23 +0200

    catch_error

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,5 @@
 export const refs = {
   galleryListEl: document.querySelector('.js-gallery'),
   formEl: document.querySelector('.js-search-form'),
+  paginationsEl: document.querySelector('.tui-pagination'),
 };
